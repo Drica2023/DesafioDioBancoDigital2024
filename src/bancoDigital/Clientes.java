c0d80855788609bc3d1202bb6a45c169846cbd0f
Drica2023 <adrianamedeiros.dev@outlook.com>
2024-11-23 18:44:12 -0300

    Projeto Banco Digital

---
+++ src/bancoDigital/Clientes.java
@@ -0,0 +1,15 @@
+package bancoDigital;
+
+public class Clientes {
+	
+	public String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ src/bancoDigital/Conta.java
@@ -0,0 +1,32 @@
+package bancoDigital;
+
+public class Conta {
+	public int agencia;
+	public int numero;
+	public double saldo;
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void sacar() {
+
+	}
+
+	public void depositar() {
+
+	}
+
+	public void transferir() {
+
+	}
+
+}
